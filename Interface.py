4962f5ba13f00ae8ce2b4f3751cbea1ef5c2c532
roeybarda <roeybarda@microsoft.com>
2022-03-05 21:38:46 +0200

    added requirements.txt

--- Interface.py
+++ Interface.py
@@ -10,24 +10,24 @@ from modules.GraphGenerator.MongoConnector import MongoConnector
 def run():
     connector = MongoConnector()
     while True:
-        print(f"Hello, choose which operation you want to make:")
-        print(f"1. Run BFS on target user")
-        print(f"2. Clean all collections")
-        print(f"3. Quit")
-        operation = input("Enter your choice (1/2/3): ")
+        print('Hello, choose which operation you want to make:')
+        print('1. Run BFS on target user')
+        print('2. Clean all collections')
+        print('3. Quit')
+        operation = input('Enter your choice (1/2/3): ')
 
         if operation == '1':
             # BFS
-            logging.info("Starting BFS operation")
-            print("Please wait, it may take a while...")
+            logging.info('Starting BFS operation')
+            print('Please wait, it may take a while...')
             startTime = timer()
             BfsExecutor(connector, 10, 3)
             endTime = timer()
-            logging.info(f"Finished BFS operation, it took {endTime - startTime} seconds")
+            logging.info(f'Finished BFS operation, it took {endTime - startTime} seconds')
         if operation == '2':
             connector.deleteAllCollections()
         if operation == '3':
-            print("Bye!")
+            print('Bye!')
             break
 
         print('Operation is finished')
@@ -36,6 +36,6 @@ def run():
 
 if __name__ == '__main__':
     logging.basicConfig(filename=f'logs/logsOfRun{datetime.now().timestamp()}.log', level=logging.INFO)
-    logging.info(f"The app is running, started at {datetime.now()}")
+    logging.info(f'The app is running, started at {datetime.now()}')
     run()
-    logging.info(f"The app has stopped, started at {datetime.now()}")
+    logging.info(f'The app has stopped, started at {datetime.now()}')
